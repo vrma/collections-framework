63136636e9f89e0e4fb2a0e11a96309cb738f0d8
vrma <vrmachado@gmail.com>
2026-04-23 14:27:58 +0200

    antes de comenzar a crear colecciones

--- collections-framework/src/main/java/com/example/App.java
+++ collections-framework/src/main/java/com/example/App.java
@@ -1,10 +1,12 @@
 package com.example;
 
 /**
- * Hello world!
+ * El Framework de Collecciones:
+ * 
+ *  https://docs.oracle.com/javase/tutorial/collections/index.html
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
