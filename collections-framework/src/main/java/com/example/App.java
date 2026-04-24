808c80bfd78f2888eafd7c9f69617b4d6082bd52
vrma <vrmachado@gmail.com>
2026-04-24 09:36:03 +0200

    actualizacion del viernes a primera hora

--- collections-framework/src/main/java/com/example/App.java
+++ collections-framework/src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * El Framework de Collecciones:
  * 
@@ -7,6 +10,19 @@ package com.example;
  */
 public class App {
     public static void main(String[] args) {
+    	
+    	/* Colleccion de numeros enteros, a partir de un array */
+    	
+    	Integer[] arrayNumerosEnteros = {1, 2, 3, 4, 5};
+    	
+    	List<Integer> numerosEnteros = Arrays.asList(arrayNumerosEnteros);
+    	
+    	/* Una coleccion obtenida directamente a partir de un array es una coleccion
+    	 * de tamaño fijo, es decir, que no se le puedan agregar ni eliminar elementos,
+    	 * pero si se pueden modificar los elementos, porque de lo contrario seria 
+    	 * inmutable*/
+    	
+    	// numerosEnteros.add(6);
         
     }
 }
