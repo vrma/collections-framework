fffd8dc3f799d45c0c65c527c419bf328c02b552
vrma <vrmachado@gmail.com>
2026-04-24 11:22:21 +0200

    antes de eliminar las variables persona1 2 3 y 4

--- collections-framework/src/main/java/com/example/App.java
+++ collections-framework/src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  *  https://docs.oracle.com/javase/tutorial/collections/index.html
  */
 public class App {
+	
 	
     public static void main(String[] args) {
     	
@@ -30,9 +32,10 @@ public class App {
     	/* Ejemplo de creacion de lista (coleccion) utilizando el metodo add(),
     	 * que es polimorfico, para agregar elementos a la lista */
     	
-    	List<Persona> personas = null;
+    	// var realmente es List<Persona>
+    	var personas = new ArrayList<Persona>();
     	
-    	Persona persona1 = Persona.builder()
+    	var persona1 = Persona.builder()
     			.nombre("Duglas")
     			.primerApellido("Taydron")
     			.segundoApellido("Gonzalez")
@@ -41,7 +44,45 @@ public class App {
     			.genero(Genero.HOMBRE)
     			.build();
     	
+    	var persona2 = Persona.builder()
+    			.nombre("Carolina")
+    			.primerApellido("Garzon")
+    			.segundoApellido("Becerra")
+    			.fechaNacimiento(LocalDate.of(2000,
+    					Month.OCTOBER, 10))
+    			.genero(Genero.MUJER)
+    			.build();
+    	
+    	var persona3 = Persona.builder()
+    			.nombre("Maria")
+    			.primerApellido("Garzon")
+    			.segundoApellido("Glez")
+    			.fechaNacimiento(LocalDate.of(2005,
+    					Month.DECEMBER, 14))
+    			.genero(Genero.MUJER)
+    			.build();
+    	
+    	var persona4 = Persona.builder()
+    			.nombre("Jeronimo")
+    			.primerApellido("Arenal")
+    			.segundoApellido("Gomez")
+    			.fechaNacimiento(LocalDate.of(1989,
+    					Month.MAY, 22))
+    			.genero(Genero.HOMBRE)
+    			.build();
+    	
+    	
     	personas.add(persona1);
+    	personas.add(persona2);
+    	personas.add(persona3);
+    	personas.add(persona4);
+    	
+    	/* En lugar de invocar el metodo add 4 veces como se ha mostrado anteriormente, 
+    	 * se podria y es preferible agregar elementos a la lista de personas de la forma
+    	 * siguiente: */
+    	
+    	personas = (ArrayList<Persona>) Arrays.asList(persona1, persona2, persona3, persona4);
+    	
     	
     	/* Si la lista de personas apunta a null, entonces no podemos agregar elementos Persona
     	 * a dicha lista, porque no hemos reservado memoria para la lista de personas 
